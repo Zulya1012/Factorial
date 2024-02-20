f493c1f48982d93772674154909bb257a1028e06
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-20 15:25:50 +0400

    Add files via upload

---
+++ fact1.java
@@ -0,0 +1,15 @@
+public class fact1{
+  public static void main(String[] arg){
+   
+    factorial(10);
+  }
+    public static void factorial(int a){
+      if (a==0)
+        return;
+      
+      System.out.println(a);
+     
+      factorial(a-1);
+    }
+}
+      
